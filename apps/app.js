7d4b5345b8809d4d66de71e5a30c26e52bbc6a58
gfed53 <gfederico7153@gmail.com>
2016-02-10 16:45:12 -0500

    Example version verbatim

--- apps/app.js
+++ apps/app.js
@@ -1,39 +1,36 @@
-var myModule = angular.module('myApp', []);
-myModule.controller('PromiseExampleCtrl', function($scope, $http, $q, $timeout){
-	function wait(){
-		var defer = $q.defer();
+// Code goes here
 
-		$timeout(function(){
-			defer.resolve();
-		}, 2000);
-
-		console.log(defer.promise);
-		console.log(defer);
-		return defer.promise;
-		
-	}
-
-	$scope.saveSettings = function(valid){
-		var url = valid ? '/api/update_password' : '/broken-api/update_password';
-		$http.put(url, $scope.data)
-		.success(function(){
-			notify();
-		})
-		.error(function(){
-			$scope.error = true;
-			notify().then(function(){
-				$scope.error = false;
-			});
+angular.module('PromisesApp', [])
+.controller('PromisesExampleCtrl', ['$scope', '$timeout', '$q', '$http', function($scope, $timeout, $q, $http) {
+	function wait() {
+		return $q(function(resolve, reject){
+		  $timeout(function() {
+			  resolve();
+      }, 2000);
 		});
+	}
+    
+  function notify() {
+    $scope.notifySaved = true;
+    return wait().then(function() {
+     $scope.notifySaved = false; 
+    });
+  }
 
-		function notify() {
-			$scope.notifySaved = true;
-			return wait().then(function(){
-				notifySaved = false;
-			});
-		}
+  $scope.saveSettings = function(valid) {
+    var url = valid ? '/api/update_password' : '/broken-api/update_password';
+		$http.put(url, $scope.data)
+    .success(function() {
+      notify();
+    })
+    .error(function() {
+      $scope.error = true;
+      notify().then(function() {
+        $scope.error = false;
+      });
+    });
 	};
-});
+}]);
 
 //Ignore
-angular.module("myApp").config(["$provide",function(a){a.decorator("$http",["$delegate","$timeout",function(a,b){return a.put=function(a){var c,d,e={success:function(a){return c=a,this},error:function(a){return d=a,this}};return b(function(){a.indexOf("broken")>=1?d():c()},3e3),e},a}])}]);
+angular.module("PromisesApp").config(["$provide",function(a){a.decorator("$http",["$delegate","$timeout",function(a,b){return a.put=function(a){var c,d,e={success:function(a){return c=a,this},error:function(a){return d=a,this}};return b(function(){a.indexOf("broken")>=1?d():c()},3e3),e},a}])}]);
